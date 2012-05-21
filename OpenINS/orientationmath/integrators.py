0f200ced8bcf0651422d5bc660424c35dc3c7152
Nickolai Novik <novikn@ukr.net>
2012-05-21 12:41:49 +0300

    wip: crazy things about INS

--- OpenINS/orientationmath/integrators.py
+++ OpenINS/orientationmath/integrators.py
@@ -2,8 +2,6 @@
 Iterative integrator implementations.
 """
 
-
-
 from abc import ABCMeta, abstractmethod
 from copy import copy
 
@@ -49,6 +47,9 @@ class TrapeziumRule(Integrator):
     """
 
     def __init__(self, init_value):
+        """
+
+        """
         super(TrapeziumRule, self).__init__(init_value)
         self._previous_value = None
 
@@ -57,29 +58,50 @@ class TrapeziumRule(Integrator):
 
         """
         if self._previous_value is not None:
-            self._accumulator += dt * (self._previous_value + sample_value) / 2
+            self._accumulator += dt * (self._previous_value + sample_value) / 2.
         else:
             self._accumulator += sample_value * dt
         self._previous_value = sample_value
         return copy(self._accumulator)
 
+class SimpsonRule(Integrator):
+    """
+    Integration by the trapezium rule.
+    """
 
-#class SimpsonRule(Integrator):
-#    """
-#    Integration by the Simpson's rule.
-#    """
-#
-#    def __init__(self, init_value):
-#        super(SimpsonRule, self).__init__(init_value)
-#        self._previous_value = None
-#
-#    def __call__(self, sample_value, dt):
-#        """
-#
-#        """
-#        if self._previous_value is not None:
-#            self._accumulator += dt * (self._previous_value + sample_value) / 2
-#        else:
-#            self._accumulator += sample_value * dt
-#        self._previous_value = sample_value
-#        return copy(self._accumulator)
+    def __init__(self, init_value):
+        """
+
+        """
+        super(SimpsonRule, self).__init__(init_value)
+        self._prev_value_1 = None
+        self._prev_value_2 = None
+        self.a2 =0.
+    def __call__(self, sample_value, dt):
+        """
+
+        """
+
+        if self._prev_value_1 is not None and self._prev_value_2 is not None:
+            self._accumulator += dt * (self._prev_value_2 +
+                                       4.*self._prev_value_1 + sample_value) / 3.
+            self._prev_value_2 =  copy(sample_value)
+            self._prev_value_1 =  None
+            self.a2 =0.
+        else:
+            if self._prev_value_2 is not None:
+
+                self._prev_value_1 =  copy(sample_value)
+                return copy(self._accumulator +self.a2 + dt * (self._prev_value_2 + sample_value) / 2.)
+            else:
+                self._prev_value_2 =  copy(sample_value)
+                self.a2 = copy(self._accumulator + sample_value * dt)
+                return copy(self._accumulator + sample_value * dt)
+
+#        self._prev_value_2 =  copy(self._prev_value_1)
+#        self._prev_value_1 =  copy(sample_value)
+
+
+
+        return copy(self._accumulator)
+
--- OpenINS/orientationmath/test/test_integrators.py
+++ OpenINS/orientationmath/test/test_integrators.py
@@ -6,8 +6,9 @@ Tests for integration methods.
 from __future__ import division
 import numpy as np
 import unittest
+import orientationmath.integrators
+from testing.inspection import get_implementations
 
-from OpenINS.orientationmath.integrators import RectangleRule, TrapeziumRule
 
 x = np.arange(0., 5, 0.0005)
 dt = x[1]-x[0]
@@ -17,15 +18,15 @@ functions = ( lambda x: x,
               lambda x: np.cos(x),
               lambda x: np.e**x )
 
-integrals = ( lambda x: 1/2 * x**2,
-              lambda x: 1/3 * x**3,
+integrals = ( lambda x: 1/2. * x**2,
+              lambda x: 1/3. * x**3,
               lambda x: np.sin(x),
               lambda x: np.e**x)
 
 
-
-methods = (RectangleRule, TrapeziumRule)
-tols = (1, 4)
+methods  = get_implementations(orientationmath.integrators,
+                               orientationmath.integrators.Integrator)
+tols = (0, 4, 4)
 
 def check_integral(method, function, integral):
     """
@@ -51,29 +52,10 @@ class IntegratorTest(unittest.TestCase):
         solution.
         """
         for  method, tol in zip (methods, tols):
+            print method
             for function, integral in zip (functions, integrals):
                 test_int, ref_int = check_integral(method, function, integral)
                 for test, ref in zip (test_int, ref_int):
                     self.assertAlmostEqual(test, ref, places=tol)
 
 
-
-#class IntegratorVectorTest(unittest.TestCase):
-#    """
-#    Test integrators with vector function.
-#    """
-#    def foo(self, x):
-#        return np.array([f(x) for f in functions])
-#
-#    def int_foo(self, x):
-#        return np.array([i(x) for i in integrals])
-#
-#    def test_integral(self):
-#        """
-#        Try to integrate each function and test result with exact
-#        solution.
-#        """
-#        for  method, tol in zip (methods, tols):
-#            test_int, ref_int = check_integral(method, self.foo, self.int_foo)
-#            for test, ref in zip (test_int, ref_int):
-#                self.assertAlmostEqual(test, ref, places=tol)
