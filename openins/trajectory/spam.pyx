1dcc4c435718fc40caa251dac49d973ee2e32d0f
Nickolai Novik <novikn@ukr.net>
2012-06-24 19:10:41 +0300

    del useless files

--- openins/trajectory/spam.pyx
+++
@@ -1,9 +0,0 @@
-from trajectory.volume cimport cube
-
-def menu(description, size):
-    print description, ":", cube(size), \
-        "cubic metres of spam"
-
-menu("Entree", 1)
-menu("Main course", 3)
-menu("Dessert", 2)
